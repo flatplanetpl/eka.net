6bcfa7fed414c6857162ab25ece248487a483dec
MrPeKa <pekunicki@hotmail.com>
2016-11-11 17:49:58 +0100

    Added recognition of the client and logging his action.

---
+++ SignalR2/SignalR2/Extensions/ExtensionHttpContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalR2.Extensions
+{
+    public static class ExtensionHttpContext
+    {
+        public static string GetRemoteIpAddress(this IRequest request)
+        {
+            object ipAddress;
+            if (request.Environment.TryGetValue("server.RemoteIpAddress", out ipAddress))
+            {
+                return ipAddress as string;
+            }
+            return "";
+        }
+    }
+}
---
+++ SignalR2/SignalR2/Hubs/User/UsersHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SignalR2.Hubs
+{
+    public class UsersHandler
+    {
+        public Dictionary<string, string> ConnectedClients;
+        public UsersHandler()
+        {
+            ConnectedClients = new Dictionary<string, string>();
+        }
+
+        public int Size()
+        {
+            return ConnectedClients.Count;
+        }
+
+        public string GetJson()
+        {
+            return JsonConvert.SerializeObject(ConnectedClients);
+        }
+    }
+    public enum ActionType
+    {
+        Connecting,
+        Disconnecting,
+        Drawing,
+        Clearing,
+    }
+}
